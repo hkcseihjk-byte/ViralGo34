f9ec003b33a09c8634f3cb110445dc784f554917
hkcseihjk-byte <hkcseihjk@gmail.com>
2026-03-03 20:58:49 +0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10678022
+    "zoneId": 10678091
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
